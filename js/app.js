6d80ae907f6acf4e98d87cda2f13879a99aaa027
ofac <ofaczero@gmail.com>
2023-09-25 11:04:01 -0500

    feat: scroll mobile, json file

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
 
+
+    $.getJSON('json/menu.json', function(jd) {
+        $('.nav-item a.option1').text(jd.option1)
+        $('.nav-item a.option2').text(jd.option2)
+        $('.nav-item a.option3').text(jd.option3)
+        $('.nav-item a.option4').text(jd.option4)
+        $('.nav-item a.option5').text(jd.option5)
+    })
+
+    $('.nav-link').click(function(){
+        $('.navbar-collapse').collapse('hide')
+    })
+
     $('.btn-light').click(function (e) { 
         e.preventDefault()
         $(this).parent().hide()
