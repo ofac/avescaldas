86b7bc9c4cfe23aceeaacf333a4e4edb8928ed48
ofac <ofaczero@gmail.com>
2023-10-12 10:27:50 -0500

    upd: json file and consume data

--- js/app.js
+++ js/app.js
@@ -21,15 +21,28 @@ $(document).ready(function () {
          // Content Section 3
          $('.section3 h2').text(jd.record.content.section3.title)
          $('.section3 p').text(jd.record.content.section3.text)
-         $('.section3 img').attr("src", jd.record.content.section3.image)
+         $('.section3 img').attr("src", jd.content.section3.image)
          // Content Section 4
          $('.section4 h2').text(jd.record.content.section4.title)
          $('.section4 p').text(jd.record.content.section4.text)
-         // Content Section 5
+         // Content Section 4 Map
+         $('.section4 iframe').attr('src',jd.record.map.code)
+         // Map
          $('.section5 h2').text(jd.record.content.section5.title)
         //  Photos
-        console.log(Object.entries(jd.record.photos).length)
-
+        $photos = Object.entries(jd.record.photos)
+        for (let i = 0; i < $photos.length; i++) {
+            $cphoto = '<div class="card pt-3" style="width: 18rem;"> \
+                  <img src="'+$photos[i][1].image+'" class="card-img-top rounded-4 object-fit-cover" alt="..." height="200px"> \
+                  <div class="card-body"> \
+                    <h5 class="card-title">'+$photos[i][1].title+'</h5> \
+                    <p class="card-text">'+$photos[i][1].text+'</p> \
+                  </div> \
+                </div>'
+            $('.cphotos').append($cphoto)
+            //console.log(i)
+        }
+        //console.log(Object.entries(jd.photos).length)
     })
 
     $('.nav-link').click(function(){
